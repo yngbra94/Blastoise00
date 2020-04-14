5727ebbab33d82d4afc0d1f61131b3bf588ca375
sondreiveland <sondreiveland@github.com>
2020-04-14 20:17:54 +1000

    made the function to start the search when reciving start.

--- command_server_node.py
+++ command_server_node.py
@@ -9,8 +9,13 @@
     rostopic pub -1 /cmd std_msgs/String -- 'start'
     rostopic pub -1 /cmd std_msgs/String -- 'stop'
 
+    The server starts the search for the beacons when getting 'start' 
+    and searches until the map is fully explored or all beacons is found.
+    It then returns back to the original starting point. 
+    If it get 'stop' does the robot stop and restarts the same task it was doing when 'start' is sent.  
+
     Subscribed: cmd/
-    Publishes:
+    Publishes: state/, beacons_left/, returning_done/, move_base/status
 
     Created: 2020/02/04
     Author: Brendan Halloran 
@@ -21,13 +26,14 @@ import rospy
 
 from commands import Commands, RobotState
 from std_msgs.msg import String, Int16, Bool
+from std_srvs.srv import SetBool
 from actionlib_msgs.msg import GoalStatusArray,  GoalStatus
+  
 
 class command_server_node:
     def __init__(self):
         self.state = RobotState.WAITING_TO_START
         self.previusState= RobotState.WAITING_TO_START
-
         self.subscriber_command = rospy.Subscriber('cmd/', String, self.callback_command)
         self.publisher_state = rospy.Publisher('state/', String, queue_size=1)
         self.subscriber_beacons = rospy.Subscriber('beacons_left/',Int16,self.callback_beacons)
@@ -47,15 +53,13 @@ class command_server_node:
 
     # Function that detects when the robot is finished with exploring 
     def callback_goalstatus(self, data):
-       # if data.status == 3:
-        #index=str(data.status_list[0]).find("status: ")
-        i=1
-        #print "callback status"
-        #print str(data.status_list[0])[index+8]
-       # if str(data.status_list[0])[index+8]=='3':
-        #    print "3 com n"
-           # self.state = RobotState.DONE
-
+        statusText = data.status_list[0].text
+        if data.status_list[0].status == 2 and len(statusText):
+            self.state=RobotState.RETURNING
+            self.previusState=RobotState.RETURNING
+            print "robot is returning maze is finished explored"
+            
+     
             
     def callback_beacons(self,beaconsLeft):
         if beaconsLeft.data == 0 and self.state == RobotState.EXPLORING:
@@ -72,19 +76,41 @@ class command_server_node:
 
 
             
-
+    #function that controls the robot. with start or stop command
     def callback_command(self, data):
-        print "heiheis"
         command = Commands(data.data)
-        print "heihei"
-        #print command
+     
+        #start exploring the maze if not all beacons is found
         if command is Commands.START and self.previusState != RobotState.RETURNING:
             self.state = RobotState.EXPLORING
+            print "start exploring "
+            #wait to see if there is a service for exploration
+            rospy.wait_for_service('/explore/explore_service')
+            try:
+                #send start to the exploration class 
+                startFunction = rospy.ServiceProxy('/explore/explore_service',SetBool)
+                startFunction(True)
+            except rospy.ServiceException, e:
+                print "Service call failed: %s" %e
+        # start the robot and make it return if the maze is explored
         elif command is Commands.START and self.previusState == RobotState.RETURNING:
             self.state = RobotState.RETURNING
+        #stop the robot     
         elif command is Commands.STOP:
             self.state = RobotState.PAUSED
+            rospy.wait_for_service('/explore/explore_service')
+            try:
+                startFunction = rospy.ServiceProxy('/explore/explore_service',SetBool)
+                startFunction(False)
+            except rospy.ServiceException, e:
+                print "Service call failed: %s" %e
     
+
+
+
+
+
+
 if __name__ == '__main__':
     print "Starting ROS Command Server module"
     rospy.init_node('command_server_node', anonymous=True)
