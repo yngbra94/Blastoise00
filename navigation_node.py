c93ab51c2d81430dffc878439342a3c7ff18b07d
OleIdole <olemartin94@hotmail.com>
2020-04-18 17:24:47 +1000

    Update navigation_node.py

--- navigation_node.py
+++ navigation_node.py
@@ -20,6 +20,23 @@
 
     Subscribed:  state/, move_base/feedback, move_base/status, move_base_simple/goal 
     Publishes:   returning_done/, exploring_finished/
+    
+    
+    
+    
+    Message from Brendan:
+    ================================================================================================
+    You should use the topics published by the explore server itself. All of the topics/services are
+    under the "/explore/" namespace. The only topic published is an marker array called "frontiers",
+    and only when the "visualize" parameter is set to true.
+
+    I have noticed a bug that can prevent this from publishing as often as it should, so I've made a
+    quick fix and pushed the change. Try doing a git pull in your explore_lite folder and if you don't
+    get a new commit d0b130b then you might have to download the repository again.
+
+    Once you get the fix, have a look at the last message published before exploration stops and see how
+    it's different to the ones published before stopping.
+    ================================================================================================
 
 
 
