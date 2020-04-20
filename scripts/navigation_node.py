ca91a6bfbf47f58f291f0c51685e408c272e16dc
yngbra94 <yngbra94@gmail.com>
2020-04-20 20:47:00 +1000

    Added try catch on the service call and sm ecomments

--- scripts/navigation_node.py
+++ scripts/navigation_node.py
@@ -2,47 +2,19 @@
 
 """
     navigation_node.py
+    ================================================================================================
 
-    This node is responsible for controlling the navigational aspect of the turtlebot 
-    this node listens to the state/ from the commandserver node and based on this performs the set task. 
+    This node is responsible for controlling the navigational aspect of the turtlebot this node listens 
+    to the state/ from the commandserver node and based on this if performs the given task. 
     The tasks is: explore the maze with sending true to the /explore/explore_service, 
     return back to start point, and stop (pause) the robot. 
 
-
-    The code needs to implement getandchange_goal.py in a nicer way. not global variables
-    The command_server node needs to know when the maze is explored using exploring_finished/
-     and when the robot is back in start position using returning_done/
-     > store the starting position
-     > the code for starting the exploring is commented out in command server node. near the bottom
-     > Pausing / stoping of the robot is not implemented for the go back home part. 
-
-
-    NOTE!!: currently the code "getandchange_goal.py" listens to move_base/status goal status and text,
-    this should be changed to listen to marker array for frontiers as explained in message from Brendan
-    below.
-    
+    ================================================================================================
     Subscribed:  state/, move_base/feedback, move_base/status  
     Publishes:   returning_done/, exploring_finished/, move_base_simple/goal
-    
-    
-    Message from Brendan: on which node should be subscribed to in order to know when
-    exploring is finished.
-    ================================================================================================
-    You should use the topics published by the explore server itself. All of the topics/services are
-    under the "/explore/" namespace. The only topic published is an marker array called "frontiers",
-    and only when the "visualize" parameter is set to true.
-
-    I have noticed a bug that can prevent this from publishing as often as it should, so I've made a
-    quick fix and pushed the change. Try doing a git pull in your explore_lite folder and if you don't
-    get a new commit d0b130b then you might have to download the repository again.
-
-    Once you get the fix, have a look at the last message published before exploration stops and see how
-    it's different to the ones published before stopping.
     ================================================================================================
 
 """
-
-#!/usr/bin/env python
 
 import rospy # ros library for python. 
 
@@ -132,13 +104,20 @@ class navigation_node:
 
     # Make robot start exploring 
     def robot_explore(self):
-        self.startFunction(True)
-        self.set_robot_action_and_pub(RobotState.EXPLORING)
-        if(self.debug):
-            print("Exploring...")
+        # try to start the exploring. If not, cast exception
+        try:
+            self.startFunction(True)
+            # Change state to EXPLORING
+            self.set_robot_action_and_pub(RobotState.EXPLORING)
+            if(self.debug):
+                print("Exploring...")
+
+        except rospy.ServiceException, e:
+            print "Could not starte exploring. \n Service call failed: %s" %e
+
+
 
     # Return robot to home pose
-    #
     def robot_return_home(self):
         # Publish the initial pose to return home
         if not self.initPose:
@@ -156,9 +135,16 @@ class navigation_node:
     
     # Pause the current opeartion of the robot. 
     def robot_pause(self):
-        self.startFunction(False)
+        # try to stop the exploring. If not, cast exception
+        try:
+            self.startFunction(False)
+        except rospy.ServiceException, e:
+            print "Could not stop exploring. \n Service call failed: %s" %e
+        # Cancel all goals. 
         self.pub_cancel_all_goals.publish()
+        # Change state to PAUSED and publish
         self.set_robot_action_and_pub(RobotState.PAUSED)
+
         if(self.debug):
             print("Paused...")
 
@@ -171,9 +157,12 @@ class navigation_node:
 
         # Store initial pose
         if(self.poseExist == False):
+            # Set init pose
             self.initPose = data.feedback.base_position
+            # Used with debug
             if(self.debug):
                 print("Init pose saved as:  %s "% (self.initPose.pose))
+            # Pose exist flag
             self.poseExist = True
     
     # Callback for the robot state
@@ -184,7 +173,9 @@ class navigation_node:
         if (self.robotCurrentState == RobotState.RETURNING and status.status_list[0].status == 3):
             # Set action to robot is home and publish 
             self.set_robot_action_and_pub(RobotState.DONE)
+            # Publish that to returning is compleated. 
             self.pub_returning_done.publish(True)
+            # Used with debug
             if (self.debug):
                 print("Robot parked in the garage. ")
         
@@ -193,22 +184,27 @@ class navigation_node:
     # @param frontiers, The subscribed frointer data from the robot, Type MarkerArray
     def frontiers_callback(self, frontiers):
         if(frontiers.markers == []):
+
+            # Used with debug
             if(self.debug):
                 print("No more frointiers.")
+            # Change the state state to DONE and publish
             self.set_robot_action_and_pub(RobotState.DONE)
+            # Publish that the exploring it done
             self.pub_exploring_done.publish(True)
 
     
     # Sets robot current state and publishes it
     #
-    # @param newAction, the new action to be set. 
+    # @param newState, the new state to be set. 
     def set_robot_action_and_pub(self, newState):
         self.robotCurrentState = newState
         self.pub_robotCurrentState.publish(self.robotCurrentState.value)
 
 
 
-
+# Main function
+# Initalisation of the navigation node 
 if __name__ == '__main__': 
     try:
         rospy.init_node('navigation_node')
